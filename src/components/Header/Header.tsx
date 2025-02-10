a84d966952d61de1e001ec0b7d8119ccb3072b1f
a.maltsev <a.maltsev@mts-link.ru>
2025-02-10 06:24:40 +0300

    add typewriter

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,18 @@
 import './Header.css';
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
+
 export const Header = () => {
     const userName = window.Telegram.WebApp.initDataUnsafe?.user?.username
+    const [text, count] = useTypewriter({
+        words: [`Здравствуй, ${userName || 'Sherri270'}!`, 'Добро пожаловать в Hatness Shop!',],
+        loop: true,
+        delaySpeed: 800,
+        typeSpeed: 115
+    })
     return <div className="product-header-container">
         <div className="product-header-welcome">
-            Здравствуй, <span style={{
-                fontWeight: 'bold'
-            }}>{userName || 'Sherri270'}</span>!
+        <span>{text}</span>
+        <Cursor cursorColor='#F7AB0A' />
         </div>
     </div>
 }
